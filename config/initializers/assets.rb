e6c02ff7e4ed265e0bbe0aa44d7d1fbab933b482
Fauzan <fauzankhan93@gmail.com>
2014-06-15 16:51:56 +0530

    Completed the layout

---
+++ config/initializers/assets.rb
@@ -0,0 +1,4 @@
+Rails.application.config.assets.precompile += %w( blueprint/screen.css )
+Rails.application.config.assets.precompile += %w( blueprint/print.css )
+Rails.application.config.assets.precompile += %w( blueprint/ie.css )
+Rails.application.config.assets.precompile += %w( custom.css )
