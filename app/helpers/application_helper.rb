143eaa8e28c98f6832c7384c9cc2b82c1889b1d0
Fauzan <fauzankhan93@gmail.com>
2014-06-15 12:44:31 +0530

    Added title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+	#return a titler om per-page basis
+	def title
+		base_title = "Ruby on Rails sample App"
+		if @title.nil?
+			base_title
+		else
+			"#{base_title} | #{@title}"
+		end
+	end
+	
 end
