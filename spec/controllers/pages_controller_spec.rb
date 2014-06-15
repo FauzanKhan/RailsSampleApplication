fca8564e5d58af7017c5a34efbead0b43520df0a
Fauzan <fauzankhan93@gmail.com>
2014-06-15 12:28:56 +0530

    Added Navigation Pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,12 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe PagesController, :type => :controller do
+  render_views
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       expect(response).to be_success
     end
+    #it "should have the right title" do
+    #  get 'home'
+    #  expect(response).to have_selector('title', 
+    #                                    :content =>"Ruby on Rails tutorial sample app | home")
+    #end
   end
 
   describe "GET 'contact'" do
@@ -14,6 +20,23 @@ RSpec.describe PagesController, :type => :controller do
       get 'contact'
       expect(response).to be_success
     end
+    #it "should have the right title" do
+    #  get 'contact'
+    #  expect(response).to have_selector("title", 
+    #                                    :content =>"Ruby on Rails tutorial sample app | contact")
+    #end
+  end
+
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      expect(response).to be_success
+    end
+    #it "should have the right title" do
+    #  get 'about'
+    #  expect(response).to have_selector("title", 
+    #                                    :content =>"Ruby on Rails tutorial sample app | about")
+    #end
   end
 
 end
