125c848bfc94c586918eae8787f2af63b0b47ba8
Fauzan <fauzankhan93@gmail.com>
2014-07-12 00:00:16 +0530

    Handled invalid sign in

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
 
   match '/signup', to: 'users#new', via: 'get'
 
-  match '/signin', to: 'sessions#new', via: 'post'
+  match '/signin', to: 'sessions#new', via: 'get'
 
   match '/signout', to: 'sessions#destroy', via: 'delete' 
   #match '/' to:, 'pages#home', via: 'get'
