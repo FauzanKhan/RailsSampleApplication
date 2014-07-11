33978cec2efb4039e0c0721c24687786c700bc96
Fauzan <fauzankhan93@gmail.com>
2014-07-11 19:25:14 +0530

    Improved user show page and added gravatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,13 @@
 class UsersController < ApplicationController
+  
+  def show
+  	@user = User.find(8)
+  	@title = @user.name
+  end
+
   def new
   	@title = "Sign Up"
   end
 
-  def show
-  	@user = User.find(8)
-  end
+  
 end
